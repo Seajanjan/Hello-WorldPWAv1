5835ad6635957ef46150660be3d0b74a12daa4ab
Seajanjan <51320511+Seajanjan@users.noreply.github.com>
2019-06-04 12:08:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-body {
-  font-family: sans-serif;
-}
+window.onload = () => {
+  'use strict';
 
-/* Make content area fill the entire browser window */
-html,
-.fullscreen {
-  display: flex;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  width: 100%;
-}
-
-/* Center the content in the browser window */
-.container {
-  margin: auto;
-  text-align: center;
-}
-
-.title {
-  font-size: 3rem;
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
 }
