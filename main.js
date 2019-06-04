232094adef289a5f7d11ff58e411331201401292
Seajanjan <51320511+Seajanjan@users.noreply.github.com>
2019-06-04 17:29:38 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function displayNotification() {
-  if (Notification.permission == 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      reg.showNotification('Hello world!');
-    });
-  }
-}
