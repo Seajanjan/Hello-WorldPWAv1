09fe5ec56b130cd412c79c5baff553ade27712a2
Seajanjan <51320511+Seajanjan@users.noreply.github.com>
2019-06-05 13:50:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-  <script type="text/javascript">
-  // This is the "Offline copy of pages" service worker
-
-// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-
-// Check compatibility for the browser we're running this in
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js?v2', {
         scope: '.' // <--- THIS BIT IS REQUIRED
@@ -15,6 +9,3 @@ if ('serviceWorker' in navigator) {
         console.log('ServiceWorker registration failed: ', err);
     });
 }
-
-
-  </script>
