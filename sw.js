c98b55752f573ba702c0ddff2d98ab109d350be5
Seajanjan <51320511+Seajanjan@users.noreply.github.com>
2019-06-05 08:56:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,6 +28,22 @@ if (url.origin == location.origin)  {
   e.respondWith(networkAndCache(req));
     }
 });
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+async function networkAndCache(req) { 
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached; 
+  }
+}
 
 
 
